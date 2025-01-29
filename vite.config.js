f6364b2a0f32f3a67f4ac4682f66daf1112c47b0
aadityashewale <aaditya.shewale@vit.edu.in>
2025-01-29 18:19:27 +0530

    Final commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const fs = require('fs');
+
+export default {
+  build: {
+    outDir: 'build',
+    // sourcemap: true,
+  },
+  server: {
+    https: process.env.SSL_CRT_FILE
+      ? {
+          key: fs.readFileSync(process.env.SSL_KEY_FILE),
+          cert: fs.readFileSync(process.env.SSL_CRT_FILE),
+        }
+      : null,
+    allowedHosts: true,
+  },
+};
